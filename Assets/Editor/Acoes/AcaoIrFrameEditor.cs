b545b9b78a256812dd0eedc5d6d3dcf0c5de8df5
Pedro Martins <1qwertyuiop.wasd@gmail.com>
2024-03-28 21:25:57 +0000

    Adicionada Acao para ir para outra frame

---
+++ Assets/Editor/Acoes/AcaoIrFrameEditor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+
+[CustomEditor(typeof(AcaoIrFrame))]
+public class AcaoIrFrameEditor : Editor
+{
+    private string[] frames;
+
+    private FramesJogoControlador frameContr;
+
+    SerializedProperty frameDestino;
+    SerializedProperty tempoEspera;
+
+    int frameSel;
+
+    private void OnEnable()
+    {
+        if (frameContr == null)
+        {
+            frameContr = FindAnyObjectByType<FramesJogoControlador>();
+        }
+
+        frameSel = 0;
+        frames = frameContr.listaCubos.Select(x => x.name).ToArray();
+
+        frameDestino = serializedObject.FindProperty("frameDestino");
+        tempoEspera = serializedObject.FindProperty("tempoEspera");
+
+        if (frameDestino.objectReferenceValue != null)
+        {
+            frameSel = frameContr.listaCubos.FindIndex((x) => x.name == frameDestino.objectReferenceValue.name);
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+
+        frameSel = EditorGUILayout.Popup("Frame Destino: ", frameSel, frames);
+        tempoEspera.floatValue = EditorGUILayout.FloatField("Tempo Espera: ",tempoEspera.floatValue); 
+
+        if (EditorGUI.EndChangeCheck())
+        {
+            frameDestino.objectReferenceValue = frameContr.listaCubos[frameSel];
+
+            serializedObject.ApplyModifiedProperties();
+            serializedObject.Update();
+        }
+    }
+}
+
---
+++ Assets/Scripts/Jogo/Acoes/AcaoIrFrame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[AcaoAtributo("Frames/Ir Para Outra Frame")]
+public class AcaoIrFrame : JogoAcao
+{
+    public CuboFrame frameDestino;
+    public float tempoEspera;
+
+    public override void Avaliar()
+    {
+        if (tempoEspera <= 0f)
+        {
+            AdventureControlador.instancia.framesJogoControlador.IrPara(frameDestino, false);
+            sucesso = true;
+            terminou = true;
+        }
+        else
+        {
+            StartCoroutine(EsperarFrame());
+        }
+    }
+
+    private IEnumerator EsperarFrame()
+    {
+        yield return new WaitForSeconds(tempoEspera);
+
+        AdventureControlador.instancia.framesJogoControlador.IrPara(frameDestino, false);
+        sucesso = true;
+        terminou = true;
+
+        yield return null;
+    }
+}
