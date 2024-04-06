32ff4d3ea998d19a5accb4d10bbd4be0f70a3b16
Pedro Martins <1qwertyuiop.wasd@gmail.com>
2024-04-06 15:13:39 +0100

    Criada Acao para alterar o valor de uma variavel

---
+++ Assets/Editor/Acoes/AcaoAlterarVariavelEditor.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AcaoAlterarVariavel))]
+public class AcaoAlterarVariavelEditor : Editor
+{
+    SerializedProperty varId;
+    SerializedProperty varValor;
+    SerializedProperty operacao;
+
+    string[] vars;
+
+    private void OnEnable()
+    {
+        varId = serializedObject.FindProperty("variavelId");
+        varValor = serializedObject.FindProperty("valorAlterar");
+        operacao = serializedObject.FindProperty("operacao");
+
+        vars = FindAnyObjectByType<ControladorVariaveis>().listaVariaveis.Select(x => x.nome+" ("+x.tipoVariavel.ToString()+")").ToArray();
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (vars.Length == 0)
+        {
+            EditorGUILayout.HelpBox("Erro: Nao existem variaveis",MessageType.Warning);
+        }
+        else
+        {
+            EditorGUI.BeginChangeCheck();
+
+            varId.intValue = EditorGUILayout.Popup(new GUIContent("Variavel"),varId.intValue, vars);
+
+            EditorGUILayout.PropertyField(operacao);
+
+            EditorGUILayout.PropertyField(varValor);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+                serializedObject.Update();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Jogo/Acoes/AcaoAlterarVariavel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AcaoAtributo("Variavel/Alterar o Valor de Uma Variavel")]
+public class AcaoAlterarVariavel : JogoAcao
+{
+    public int variavelId;
+    public string valorAlterar;
+    public TipoOperacao operacao;
+
+    private ControladorVariaveis contr;
+
+    public override void Avaliar()
+    {
+        contr = AdventureControlador.instancia.controladorVariaveis;
+
+        if (contr.listaVariaveis.Count == 0)
+        {
+            Debug.LogError("Erro, tentou avaliar variaveis mas nao existem variaveis!");
+            sucesso = false;
+            terminou = true;
+            return;
+        }
+
+        Variavel var = contr.listaVariaveis[variavelId];
+
+        string msg = "Variavel '" + var.nome + "' alterada: " + var.valor + " -> ";
+
+        if (operacao == TipoOperacao.IGUAL_A)
+        {
+            var.valor = valorAlterar;
+        }
+        else if (operacao == TipoOperacao.SOMAR)
+        {
+            int val = int.Parse(var.valor) + int.Parse(valorAlterar);
+
+            var.valor = val.ToString();          
+        }
+
+        msg += var.valor;
+
+        Debug.Log(msg);
+
+        sucesso = true;
+        terminou = true;
+    }
+
+    public enum TipoOperacao
+    {
+        IGUAL_A,
+        SOMAR,
+        SUBTRAIR
+    }
+
+}
